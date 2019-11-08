7707dd94845c063177fb6c79dc8fa841d4a7bd9b
Dominik Spicher <dominik.spicher@inacta.ch>
2019-11-08 10:03:18 +0100

    Make script runnable by cron

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-bitcoin-cli getrawmempool true > mempool.json 2>> error.log
-/usr/bin/python3 /home/spd/mempool-rbf/analyzer.py >> rbf.log 2>> error.log
+/usr/local/bin/bitcoin-cli getrawmempool true > mempool.json 2>> /home/spd/mempool-rbf/error.log
+/usr/bin/python3 /home/spd/mempool-rbf/analyzer.py >> /home/spd/mempool-rbf/rbf.log 2>> /home/spd/mempool-rbf/error.log
