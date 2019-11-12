6a8fb8c58fc33bf56a30898872a65a59f6ef3492
Dominik Spicher <dominik.spicher@inacta.ch>
2019-11-12 11:30:24 +0100

    Fix mempool file location bug

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/bitcoin-cli getrawmempool true > mempool.json 2>> /home/spd/mempool-rbf/error.log
+/usr/local/bin/bitcoin-cli getrawmempool true > /home/spd/mempool-rbf/mempool.json 2>> /home/spd/mempool-rbf/error.log
 /usr/bin/python3 /home/spd/mempool-rbf/analyzer.py >> /home/spd/mempool-rbf/rbf.log 2>> /home/spd/mempool-rbf/error.log
